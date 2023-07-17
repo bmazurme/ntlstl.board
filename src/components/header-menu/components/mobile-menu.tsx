0eac4e8df6fbc4f47177cd938daae42a50b3990d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-07-17 08:14:01 +0300

    fix: header-menu

---
+++ src/components/header-menu/components/mobile-menu.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
+
+import IconButton from '../../icon-button';
+import Modal from '../../modal';
+
+import { useModal } from '../../../hooks/use-modal';
+
+export default function MobileMenu() {
+  const { isModalOpen, openModal, closeModal } = useModal();
+
+  return (
+    <>
+      <IconButton component={ChevronDownIcon} handler={openModal}/>
+      {isModalOpen
+        && (<Modal isOpen={isModalOpen} onClose={closeModal} children={<div>menu</div>} />)}
+    </>
+  );
+}
--- src/components/header-menu/header-menu.tsx
+++ src/components/header-menu/header-menu.tsx
@@ -1,12 +1,13 @@
-/* eslint-disable max-len */
 import React from 'react';
 import { GroupBase, OptionsOrGroups, PropsValue } from 'react-select';
 import { PlusIcon } from '@heroicons/react/24/outline';
 
 import Button from '../button';
 import CustomSelect from '../custom-select';
+import MobileMenu from './components/mobile-menu';
 
 import { useAppSelector, useAppDispatch } from '../../hooks';
+import useWindowDimensions, { getVisualProps } from '../../hooks/use-window-dimensions';
 import {
   selectWorkplaces, setWorkplaces, selectWorkplace, setWorkplace,
 } from '../../store/slices';
@@ -14,6 +15,9 @@ import {
 import style from './header-menu.module.css';
 
 export default function HeaderMenu() {
+  const { blocks } = getVisualProps(useWindowDimensions());
+  const isMobile = blocks === 1;
+
   const dispatch = useAppDispatch();
   const project = useAppSelector(selectWorkplace) as unknown as PropsValue<string>;
   const projects = useAppSelector(selectWorkplaces) as unknown as OptionsOrGroups<string, GroupBase<string>>;
@@ -26,13 +30,15 @@ export default function HeaderMenu() {
   ));
 
   return (
-    <div className={style.header_menu}>
-      <CustomSelect
-        options={projects}
-        value={project}
-        onChange={(pr) => dispatch(setWorkplace(pr))}
-      />
-      <Button handler={addProject} title="Add project" icon={PlusIcon} />
-    </div>
+    isMobile
+      ? <MobileMenu />
+      : <div className={style.header_menu}>
+          <CustomSelect
+            options={projects}
+            value={project}
+            onChange={(pr) => dispatch(setWorkplace(pr))}
+          />
+          <Button handler={addProject} title="Add project" icon={PlusIcon} />
+        </div>
   );
 }
