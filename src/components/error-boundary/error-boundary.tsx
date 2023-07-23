fd2c243c5107d77e9f61d47573533996a165a96a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-07-23 11:56:46 +0300

    feat: error-boundary

---
+++ src/components/error-boundary/error-boundary.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import { ErrorBoundary, type FallbackProps } from 'react-error-boundary';
+import { useNavigate } from 'react-router-dom';
+
+type ErrorBoundaryWrapperProps = PropsWithChildren<unknown>;
+
+import style from './error-boundary.module.css';
+
+function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
+  const navigate = useNavigate();
+
+  const onReset = () => {
+    resetErrorBoundary();
+    navigate('/');
+  };
+
+  return (
+    <div className={style['error-boundary']}>
+      <h2 className={style.title}>APP-ERROR</h2>
+      <p className={style.message}>{error.message}</p>
+      <div className={style.block}>
+        Try to
+        <button className={style.button} onClick={resetErrorBoundary}>
+          Reload app
+        </button>
+        or
+        <button className={style.link} onClick={onReset}>
+          Go to homepage
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default function ErrorBoundaryWrapper({ children, }: ErrorBoundaryWrapperProps) {
+  return (
+    <ErrorBoundary onReset={() => console.log('reset')} FallbackComponent={ErrorFallback}>
+      {children}
+    </ErrorBoundary>
+  );
+}
