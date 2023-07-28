4b6e9f8701b35dc0b894396924dc087b01e2dc72
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-07-28 08:23:40 +0300

    fix: server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,21 +1,30 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import { config as dotEnvConfig } from 'dotenv';
 import http from 'http';
 import WebSocket from 'ws';
 
 import index from './routes';
+import onConnection from './socket';
 import { NotFoundError } from './errors';
 
 import { errorLogger } from './middlewares/logger';
-
 import errorHandler from './middlewares/error-handler';
+
+import { helmetConfig } from './utils/helmet-config';
+import { corsOptions } from './utils/cors-options';
 
 dotEnvConfig();
 
-const app = express();
 const port = process.env.PORT ?? 3001;
+
+const app = express();
+
+app.use(cors(corsOptions));
+app.use(helmet.contentSecurityPolicy(helmetConfig));
 
 // initialize a simple http server
 const server = http.createServer(app);
@@ -23,19 +32,7 @@ const server = http.createServer(app);
 // initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws: WebSocket) => {
-  // connection is up, let's add a simple simple event
-  ws.on('message', (message: string) => {
-    // log the received message and send it back to the client
-    console.log('received: %s', message);
-    ws.send(`Hello, you sent -> ${message}`);
-  });
-
-  // send immediatly a feedback to the incoming connection
-  ws.send(JSON.stringify([{
-    id: 0, title: 'title', notification: 'text', read: false,
-  }]));
-});
+wss.on('connection', (ws: WebSocket) => onConnection(ws));
 
 app.use('/static', express.static(path.resolve(process.cwd(), 'static')));
 
