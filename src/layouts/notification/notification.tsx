9846a70cf135960e23d71ea1a3bfe15d8d0d626e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-07-28 08:22:26 +0300

    fix: notification

---
+++ src/layouts/notification/notification.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { InformationCircleIcon, CheckCircleIcon } from '@heroicons/react/24/outline';
+
+import { useAppSelector } from '../../hooks';
+import { selectNotification } from '../../store/slices';
+
+import style from './notification.module.css';
+
+export default function Notification() {
+  const data = useAppSelector(selectNotification);
+  console.log(data);
+
+  return (
+    <>
+      <h2>Notification</h2>
+      {data.map((item) => (
+        <div key={uuidv4()}>
+          <div className={style.icon}>
+            <InformationCircleIcon className="h-6 w-6" />
+          </div>
+          <div className={style.icon}>
+            <CheckCircleIcon className="h-6 w-6" />
+          </div>
+
+          <h3>{item.title}</h3>
+          <p>{item.notification}</p>
+        </div>
+      ))}
+    </>
+  );
+}
