846ffe8f5d5dc172fe3014e0e916fc9c076b4859
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-07-28 08:23:01 +0300

    fix: support

---
+++ src/layouts/support/support.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Container from '../../components/container';
+
+import style from './support.module.css';
+
+function SupportBody() {
+  return (
+    <>
+      <h2>Support</h2>
+    </>
+  );
+}
+
+export default function Support() {
+  return (<Container header children={<SupportBody />} />);
+}
