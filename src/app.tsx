9c071a9cdf0a4dd6a1aeb47fceda70bab063db2a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-07-28 08:18:40 +0300

    fix: app

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { TouchBackend } from 'react-dnd-touch-backend';
 
 import MainPage from './pages/main-page';
+import ChartPage from './pages/chart-page';
+import SupportPage from './pages/support-page';
+import ProfileModalPage from './pages/profile-modal-page';
+import SupportModalPage from './pages/support-modal-page';
+import UsersModalPage from './pages/users-modal-page';
 import NotFoundPage from './pages/not-found-page';
 
-import { useAppSelector } from './hooks';
 import { selectHistory } from './store/slices';
+import ThemeContext from './context/theme-context';
 
+import { useAppSelector } from './hooks';
 import useWindowDimensions, { getVisualProps } from './hooks/use-window-dimensions';
 import useDarkTheme from './hooks/use-dark-theme';
-
-import ThemeContext from './context/theme-context';
 
 import { Urls } from './utils';
 
@@ -23,15 +27,32 @@ export default function App() {
   const history = useAppSelector(selectHistory);
   const isMobile = blocks === 1;
 
+  const location = useLocation();
+
+  useEffect(() => {
+    location.state = null;
+  }, []);
+
   console.log(history);
 
   return (
     <ThemeContext.Provider value={providerValue}>
       <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
-        <Routes>
+        <Routes location={location.state?.pathname || location}>
           <Route index element={(<MainPage />)} />
+          <Route path={Urls.SUPPORT.INDEX} element={(<SupportPage />)} />
+          <Route path={Urls.CHART.INDEX} element={(<ChartPage />)} />
           <Route path={Urls[404]} element={(<NotFoundPage />)} />
         </Routes>
+
+        {location.state?.pathname
+          && (
+            <Routes>
+              <Route path={Urls.SUPPORT.INDEX} element={(<SupportModalPage />)} />
+              <Route path={Urls.PROFILE.INDEX} element={(<ProfileModalPage />)} />
+              <Route path={Urls.USERS.INDEX} element={(<UsersModalPage />)} />
+            </Routes>
+          )}
       </DndProvider>
     </ThemeContext.Provider>
   );
