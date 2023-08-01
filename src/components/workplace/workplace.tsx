d5f986818b604d27632aa9b9cb28e94cf775d2d5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-01 08:24:36 +0300

    fix: workplace

--- src/components/workplace/workplace.tsx
+++ src/components/workplace/workplace.tsx
@@ -5,13 +5,21 @@ import WorkplaceHeader from '../workplace-header';
 import WorkplaceTools from '../workplace-tools';
 
 import style from './workplace.module.css';
+import { useAppSelector } from '../../hooks';
+import { selectBookId } from '../../store/slices';
 
 export default function Workplace() {
+  const data = useAppSelector(selectBookId);
+
   return (
     <div className={style.workplace}>
-      <WorkplaceHeader />
-      <Blocks />
-      <WorkplaceTools />
+      {data && (
+        <>
+          <WorkplaceHeader />
+          <Blocks />
+          <WorkplaceTools />
+        </>
+      )}
     </div>
   );
 }
