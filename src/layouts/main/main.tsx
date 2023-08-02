4382e84e9ea42b23d0a2099d9a6ec0439f500fa2
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-02 08:11:51 +0300

    fix: main

--- src/layouts/main/main.tsx
+++ src/layouts/main/main.tsx
@@ -3,14 +3,11 @@ import React from 'react';
 import Container from '../../components/container';
 import Workplace from '../../components/workplace';
 
-// import Preloader from '../../components/preloader';
-
 import style from './main.module.css';
 
 export default function Main() {
   return (
     <div className={style.app}>
-      {/* <Preloader /> */}
       <Container sidebar header children={<Workplace />} />
     </div>
   );
