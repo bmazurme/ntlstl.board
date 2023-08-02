47b78c406add0b9cb331375a2d83d7e99b7be29e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-02 08:11:37 +0300

    fix: with-user

--- src/hocs/with-user.tsx
+++ src/hocs/with-user.tsx
@@ -1,11 +1,14 @@
 import React, { type ComponentType } from 'react';
 
+import { useGetUserMeQuery } from '../store/api';
+
 export default function withUser<P extends Record<string, unknown>>(
   Page: ComponentType<P>,
   shouldBeAuthorized = false,
-  ) {
+) {
   return function WithUser(pageProps: P) {
-    const pagePropsWithUser = { ...pageProps };
+    const { data: user } = useGetUserMeQuery() as { data: TypeUser };
+    const pagePropsWithUser = { ...pageProps, user };
 
     if (shouldBeAuthorized) {
       return <div>Something went wrong</div>;
