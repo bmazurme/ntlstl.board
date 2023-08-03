074330367f48e85a6c3ec076b0b54104d872e42e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-03 08:15:17 +0300

    fix: workplace-form

--- src/components/workplace-form/workplace-form.tsx
+++ src/components/workplace-form/workplace-form.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useAppSelector } from '../../hooks';
-import { selectBook } from '../../store/slices';
+import { selectBookId } from '../../store/slices';
 
 import Settings from '../../layouts/settings';
 import { values } from '../../mocks/values';
@@ -8,7 +8,7 @@ import { values } from '../../mocks/values';
 import style from './workplace-form.module.css';
 
 export default function WorkplaceForm() {
-  const { name } = useAppSelector(selectBook);
+  const { name } = useAppSelector(selectBookId)!;
   return (
     <>
       <h2>{name}</h2>
