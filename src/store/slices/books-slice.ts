058c39942f95f324fd8c9a91c6e3554bc9457879
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-03 08:08:14 +0300

    fix: books-slice

--- src/store/slices/books-slice.ts
+++ src/store/slices/books-slice.ts
@@ -3,13 +3,12 @@ import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
 import { RootState } from '..';
+import { bookApiEndpoints } from '../api/book-api/endpoints';
 
-type TypeBook = Record<string, string>;
 type TypeBooksState = { data: TypeBook[] };
-
 // https://redux-toolkit.js.org/rtk-query/usage/examples
 const initialState: TypeBooksState = {
-  data: [{ name: 'Test', id: 'test' }],
+  data: [],
 };
 
 const slice = createSlice({
@@ -20,6 +19,35 @@ const slice = createSlice({
       state,
       { payload: data }: PayloadAction<TypeBook>,
     ) => ({ ...state, data: [...state.data, data] }),
+  },
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(
+        bookApiEndpoints.endpoints.getBookById.matchFulfilled,
+        (state, action) => ({
+          ...state,
+          data: action.payload,
+        }),
+      )
+      .addMatcher(
+        bookApiEndpoints.endpoints.getBookById.matchRejected,
+        (state, action) => {
+          console.log('rejected', state, action);
+        },
+      )
+      .addMatcher(
+        bookApiEndpoints.endpoints.addBook.matchFulfilled,
+        (state, action) => ({
+          ...state,
+          data: action.payload,
+        }),
+      )
+      .addMatcher(
+        bookApiEndpoints.endpoints.addBook.matchRejected,
+        (state, action) => {
+          console.log('rejected', state, action);
+        },
+      );
   },
 });
 
