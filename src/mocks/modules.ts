59b2025d3122deb0ce2e970e8041f32d4fafe820
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-03 08:11:35 +0300

    fix: mocks

--- src/mocks/modules.ts
+++ src/mocks/modules.ts
@@ -1,6 +1,8 @@
+import { TypeModule } from '../store/slices';
+
 const modules = [
   { value: '0', label: 'WS' },
   { value: '1', label: 'HVAC' },
-];
+] as unknown as TypeModule[];
 
 export { modules };
