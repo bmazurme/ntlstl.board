d83d07c91091046f1aa311fab0edcb57d7e7e51d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-04 08:27:23 +0300

    fix: not-found-page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import NotFound from '../../layouts/not-found';
+import NotFound from '../../layouts/not-found-layout';
 
 export default function NotFoundPage() {
   return (<NotFound />);
