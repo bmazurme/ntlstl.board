2f940511fc13bc490523824b57aee3e68f2f6cb5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-04 08:24:48 +0300

    fix: user-modal-page

--- src/pages/users-modal-page/users-modal-page.tsx
+++ src/pages/users-modal-page/users-modal-page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import UsersModal from '../../layouts/users-modal';
+import UsersModal from '../../layouts/users-modal-layout';
 
 export default function UsersModalPage() {
   return (<UsersModal />);
