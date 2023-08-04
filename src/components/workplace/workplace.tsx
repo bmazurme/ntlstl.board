d65c250b0821e7671dc2de384338cf86c66be2f6
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-04 08:26:15 +0300

    fix: workplace

--- src/components/workplace/workplace.tsx
+++ src/components/workplace/workplace.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Outlet, useParams } from 'react-router';
 
 import Blocks from '../blocks';
 import WorkplaceHeader from '../workplace-header';
 import WorkplaceTools from '../workplace-tools';
 
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { selectCurrentBook, setCurrentBook, selectBooks } from '../../store/slices';
+import { useGetBlocksByIdMutation } from '../../store/api';
+
 import style from './workplace.module.css';
-import { useAppSelector } from '../../hooks';
-import { selectBookId } from '../../store/slices';
 
-export default function Workplace() {
-  const data = useAppSelector(selectBookId);
-
+export function Project() {
+  const data = useAppSelector(selectCurrentBook);
   return (
-    <div className={style.workplace}>
+    <>
       {data && (
         <>
           <WorkplaceHeader />
@@ -20,6 +22,31 @@ export default function Workplace() {
           <WorkplaceTools />
         </>
       )}
+    </>
+  );
+}
+
+export default function Workplace() {
+  const dispatch = useAppDispatch();
+  const [blocksById] = useGetBlocksByIdMutation();
+  const books = useAppSelector(selectBooks);
+  const { bookId } = useParams();
+  const book = books?.find((x) => x.id === bookId);
+
+  useEffect(() => {
+    const getData = async () => {
+      if (book) {
+        await blocksById(book.id);
+        dispatch(setCurrentBook(book));
+      }
+    };
+
+    getData();
+  }, [book?.id]);
+
+  return (
+    <div className={style.workplace}>
+      {book && (<Outlet />)}
     </div>
   );
 }
