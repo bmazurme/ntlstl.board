dbd64de72dfa63884df077adb4b08a6d8337b6d5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-08 08:14:59 +0300

    fix: block-api

--- src/store/api/blocks-api/endpoints/index.ts
+++ src/store/api/blocks-api/endpoints/index.ts
@@ -1,5 +1,11 @@
 import blockApi from '..';
 
+type TypeTest = {
+  currentItem: TypeItem & { currentColumnIndex: number, id: string };
+  columnName: number;
+  id: string;
+};
+
 const blocksApiEndpoints = blockApi
   .enhanceEndpoints({
     addTagTypes: ['blocks'],
@@ -20,8 +26,66 @@ const blocksApiEndpoints = blockApi
         }),
         invalidatesTags: ['blocks'],
       }),
+      updateBlocks: builder.mutation<TypeBlock, TypeTest>({
+        query: (data) => ({
+          url: '/api/blocks',
+          method: 'PATCH',
+          body: data,
+        }),
+        invalidatesTags: ['blocks'],
+      }),
+      removeItem: builder.mutation<TypeBlock, any>({
+        query: (data) => ({
+          url: '/api/blocks',
+          method: 'PUT',
+          body: data,
+        }),
+        invalidatesTags: ['blocks'],
+      }),
+      removeBlock: builder.mutation<TypeBlock, any>({
+        query: (data) => ({
+          url: '/api/blocks/book',
+          method: 'PUT',
+          body: data,
+        }),
+        invalidatesTags: ['blocks'],
+      }),
+      addBlock: builder.mutation<TypeBlock, any>({
+        query: (data) => ({
+          url: '/api/blocks',
+          method: 'POST',
+          body: data,
+        }),
+        invalidatesTags: ['blocks'],
+      }),
+      renameBlocks: builder.mutation<TypeBlock, any>({
+        query: (data) => ({
+          url: '/api/blocks/rename',
+          method: 'PATCH',
+          body: data,
+        }),
+        invalidatesTags: ['blocks'],
+      }),
+      setMovedBlock: builder.mutation<TypeBlock, any>({
+        query: (data) => ({
+          url: '/api/blocks/set-moved-block',
+          method: 'PATCH',
+          body: data,
+        }),
+        invalidatesTags: ['blocks'],
+      }),
+      // setMovedBlock
     }),
   });
 
-export const { useGetBlocksQuery, useGetBlocksByIdMutation } = blocksApiEndpoints;
+export const {
+  useGetBlocksQuery,
+  useGetBlocksByIdMutation,
+  useUpdateBlocksMutation,
+  useRemoveItemMutation,
+  useRemoveBlockMutation,
+  useAddBlockMutation,
+  useRenameBlocksMutation,
+  useSetMovedBlockMutation,
+} = blocksApiEndpoints;
 export { blocksApiEndpoints };
