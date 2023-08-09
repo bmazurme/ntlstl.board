61bd836a75df7f0e9bed84413f7ae1c3a925b0a5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-09 08:19:04 +0300

    fix: signin-layout

--- src/layouts/signin-layout/signin-layout.tsx
+++ src/layouts/signin-layout/signin-layout.tsx
@@ -1,36 +1,41 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-import Footer from './components/footer';
+import InputField from '../../components/input-field';
+import Footer from '../../components/footer';
+
+import { Urls } from '../../utils';
 
 import style from './signin-layout.module.css';
 
 export default function SignInLayout() {
   const [errors] = useState({ login: '123456', password: '234567' });
+  const links = [
+    {
+      id: uuidv4(), help: 'Нет аккаунта?', to: Urls.SIGN.UP, label: 'Зарегистрироваться',
+    },
+    {
+      id: uuidv4(), help: 'Забыли пароль?', to: Urls.PASSWORD.FORGOT, label: 'Восстановить',
+    },
+    {
+      id: uuidv4(), help: 'Войти по', to: Urls.BASE.INDEX, label: 'OAUTH',
+    },
+  ];
 
   return (
     <div className={style.layout}>
       <div className={style.container}>
-        <h2 className={style.title}>Войти</h2>
+        <h2 className={style.title}>Вход</h2>
 
         <form className={style.form}>
-          <div className={style.boxes}>
-            <div className={style.box}>
-              <input className={style.input} type="text" />
-              <span className={style.error}>
-                {errors.login}
-              </span>
-            </div>
-            <div className={style.box}>
-              <input className={style.input} type="password" />
-              <span className={style.error}>
-                {errors.password}
-              </span>
-            </div>
+          <div className={style.fields}>
+            <InputField name="login" type="text" errors={errors} />
+            <InputField name="password" type="password" errors={errors} />
           </div>
           <button className={style.button} type="submit">Войти</button>
         </form>
 
-        <Footer />
+        <Footer links={links} />
       </div>
     </div>
   );
