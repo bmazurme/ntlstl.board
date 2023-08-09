41e3416331910d21af89029fa01d9812177d211a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-09 08:13:08 +0300

    fix: footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import style from './footer.module.css';
+
+export default function Footer({ links }: { links: Record<string, string>[] }) {
+  return (
+    <ul className={style.footer}>
+      {links.map((link) => (
+        <li className={style.item} key={link.id}>
+          {link.help}
+          <Link to={link.to} className={style.link}>{link.label}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
