e7f15a9bb02e45b7728400d532b70f38ee6cecad
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-09 08:17:38 +0300

    fix: items-api

---
+++ src/store/api/items-api/endpoints/index.ts
@@ -0,0 +1,65 @@
+import itemsApi from '..';
+
+// type TypeTest = {
+//   currentItem: TypeItem & { currentColumnIndex: number, id: string };
+//   columnName: number;
+//   id: string;
+// };
+
+const itemsApiEndpoints = itemsApi
+  .enhanceEndpoints({
+    addTagTypes: ['blocks'],
+  })
+  .injectEndpoints({
+    endpoints: (builder) => ({
+      removeItem: builder.mutation<TypeBlock, any>({
+        query: (data) => ({
+          url: '/api/items',
+          method: 'DELETE',
+          body: data,
+        }),
+        invalidatesTags: ['blocks'],
+      }),
+      setMovedItem: builder.mutation<TypeBlock, any>({
+        query: (data) => ({
+          url: '/api/items',
+          method: 'PATCH',
+          body: data,
+        }),
+        invalidatesTags: ['blocks'],
+      }),
+      changeItemValues: builder.mutation<TypeBlock, any>({
+        query: (data) => ({
+          url: '/api/items/values',
+          method: 'PATCH',
+          body: data,
+        }),
+        invalidatesTags: ['blocks'],
+      }),
+      changeItemValue: builder.mutation<TypeBlock, any>({
+        query: (data) => ({
+          url: '/api/items/value',
+          method: 'PATCH',
+          body: data,
+        }),
+        invalidatesTags: ['blocks'],
+      }),
+      getItemResult: builder.mutation<TypeBlock, any>({
+        query: (data) => ({
+          url: '/api/items/result',
+          method: 'PATCH',
+          body: data,
+        }),
+        invalidatesTags: ['blocks'],
+      }),
+    }),
+  });
+
+export const {
+  useRemoveItemMutation,
+  useSetMovedItemMutation,
+  useChangeItemValuesMutation,
+  useChangeItemValueMutation,
+  useGetItemResultMutation,
+} = itemsApiEndpoints;
+export { itemsApiEndpoints };
