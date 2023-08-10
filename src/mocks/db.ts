77805b9285794b1f5a25458c5cf67bdc091c8f82
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-10 08:19:52 +0300

    fix: mocks-db

--- src/mocks/db.ts
+++ src/mocks/db.ts
@@ -1,27 +1,27 @@
-type TWorkPlace = {
-  id: string;
-  name: string;
-  ownerId: string;
-  users: string[];
-  blocks: TBlock[];
-};
+// type TWorkPlace = {
+//   id: string;
+//   name: string;
+//   ownerId: string;
+//   users: string[];
+//   blocks: TypeBlock[];
+// };
 
-type TBlock = {
-  id: string;
-  workplaceId: string;
-  index: number;
-  name: string;
-  items: TItem[];
-};
+// type TBlock = {
+//   id: string;
+//   workplaceId: string;
+//   index: number;
+//   name: string;
+//   items: TypeItem[];
+// };
 
-type TItem = {
-  id: string;
-  index: number;
-  values: TValue[];
-};
+// type TItem = {
+//   id: string;
+//   index: number;
+//   values: TypeValue[];
+// };
 
-type TValue = {
-  name: string;
-  value: number;
-  color: string;
-};
+// type TValue = {
+//   name: string;
+//   value: number;
+//   color: string;
+// };
