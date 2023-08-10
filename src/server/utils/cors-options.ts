8e170521337ff1530a6169d5bade9133f95aa30c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-10 08:15:23 +0300

    fix: cors-options

--- src/server/utils/cors-options.ts
+++ src/server/utils/cors-options.ts
@@ -2,7 +2,6 @@ import cors from 'cors';
 
 import { ALLOWED_HEADERS, METHODS, WHITE_LIST } from './constants';
 
-
 const checkWhiteList: any = (origin: string, callback: any) => {
   if (WHITE_LIST.indexOf(origin) !== -1 || !origin) {
     callback(null, true);
