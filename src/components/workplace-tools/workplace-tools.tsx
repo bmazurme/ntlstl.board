8861bbe039da3bf39f00724811f753ec19fb2a58
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-10 08:23:53 +0300

    fix: workplace

--- src/components/workplace-tools/workplace-tools.tsx
+++ src/components/workplace-tools/workplace-tools.tsx
@@ -1,34 +1,22 @@
+/* eslint-disable no-return-await */
 import React from 'react';
 import { useParams } from 'react-router';
 import { PlusIcon } from '@heroicons/react/24/outline';
 
 import Button from '../button';
 
-import { useAppSelector } from '../../hooks';
-import { selectBlocks } from '../../store/slices';
 import { useAddBlockMutation } from '../../store/api';
 
 import style from './workplace-tools.module.css';
 
 export default function WorkplaceTools() {
-  const items = useAppSelector(selectBlocks);
-  const [setBlocks] = useAddBlockMutation();
   const { bookId } = useParams();
-
-  const addBlock = () => {
-    const index = Object.keys(items).length;
-
-    const newBlock = {
-      ...items,
-      [index]: { index, name: `BLOCK${index + 1}`, items: [] },
-    };
-
-    setBlocks({ data: newBlock, bookId });
-  };
+  const [addBlocks] = useAddBlockMutation();
+  const onAddBlock = async () => await addBlocks({ bookId });
 
   return (
     <div className={style.tools}>
-      <Button icon={PlusIcon} handler={addBlock} title="Add block" />
+      <Button icon={PlusIcon} handler={onAddBlock} title="Add block" />
     </div>
   );
 }
