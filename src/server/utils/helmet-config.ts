ffd4c8b8580a0c7943541eba475e2af832f6e02f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-10 08:15:07 +0300

    fix: helmet-config

--- src/server/utils/helmet-config.ts
+++ src/server/utils/helmet-config.ts
@@ -1,11 +1,11 @@
 const helmetConfig = {
   useDefaults: true,
   directives: {
-    defaultSrc: ["'self'", 'http://localhost:3000'],
-    scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'http://localhost:3000'],
-    connectSrc: ["'self'", 'http://localhost:3000'],
-    styleSrc: ["'self'", "'unsafe-inline'", 'http://localhost:3000'],
-    imgSrc: ["'self'", 'http://localhost:3000'],
+    defaultSrc: ["'self'", 'http://localhost:3000', 'https://oauth.yandex.ru/token'],
+    scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'http://localhost:3000', 'https://oauth.yandex.ru/token'],
+    connectSrc: ["'self'", 'http://localhost:3000', 'https://oauth.yandex.ru/token'],
+    styleSrc: ["'self'", "'unsafe-inline'", 'http://localhost:3000', 'https://oauth.yandex.ru/token'],
+    imgSrc: ["'self'", 'http://localhost:3000', 'https://oauth.yandex.ru/token'],
   },
 };
 
