444102e3123d1fca9e37454fa856d001e77b1cc5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-10 08:13:16 +0300

    fix: dev-config

---
+++ src/utils/dev-config.ts
@@ -0,0 +1,7 @@
+const DEV_JWT_SECRET = 'super-strong-secret';
+
+const CLIENT_ID = 'c709762dfe3e447999beb343da0bee9f';
+const CLIENT_SECRET = 'c1ff76a0170e4f49b9572665a233d901';
+
+export default DEV_JWT_SECRET;
+export { CLIENT_ID, CLIENT_SECRET };
