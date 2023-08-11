969040b9dd7b01f3efba76c0dbefb617cc448faa
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-11 08:11:54 +0300

    fix: oauth-controller

--- src/server/controllers/oauth-controller/index.ts
+++ src/server/controllers/oauth-controller/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable import/no-extraneous-dependencies */
 import { NextFunction, Request, Response } from 'express';
 import { config as dotEnvConfig } from 'dotenv';
@@ -7,19 +8,20 @@ import { users } from '../../mocks/db';
 
 dotEnvConfig();
 
-const oauthYaSigninController = (req: Request, res: Response, next: NextFunction) => {
-  const { token } = req.body;
-  const requestOptions: RequestInit = {
-    method: 'POST',
-    headers: { Authorization: `Bearer ${token}` },
-    redirect: 'follow',
-  };
+const oauthYaSigninController = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { token } = req.body;
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: { Authorization: `Bearer ${token}` },
+      redirect: 'follow',
+    };
 
-  fetch('https://login.yandex.ru/info', requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
+    const response = await fetch('https://login.yandex.ru/info', requestOptions);
+
+    if (response.ok) {
+      const result = await response.json();
       const { default_email } = result;
-
       const user = users.find((u) => u.defaultEmail === default_email);
 
       if (!user) {
@@ -32,15 +34,17 @@ const oauthYaSigninController = (req: Request, res: Response, next: NextFunction
         { expiresIn: '7d' },
       );
 
-      console.log(tokenNew);
-
       return res.cookie('token', tokenNew, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: true,
       }).send(result);
-    })
-    .catch((error) => console.log('error', error));
+    }
+
+    return res.send({ message: 'error' });
+  } catch (err) {
+    next(err);
+  }
 };
 
 export { oauthYaSigninController };
