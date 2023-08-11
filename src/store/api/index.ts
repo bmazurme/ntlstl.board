83877ad3d77c38166c8af00dc3574c3540772e02
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-11 08:11:22 +0300

    fix: api

--- src/store/api/index.ts
+++ src/store/api/index.ts
@@ -1,4 +1,5 @@
 export * from './auth-api/endpoints';
+export * from './oauth-api/endpoints';
 export * from './blocks-api/endpoints';
 export * from './items-api/endpoints';
 export * from './book-api/endpoints';
