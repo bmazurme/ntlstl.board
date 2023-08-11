a35788869d2133cb4990715cf448761f3f0317eb
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-11 15:21:19 +0300

    fix: oauth-layout

--- src/layouts/oauth-layout/oauth-layout.tsx
+++ src/layouts/oauth-layout/oauth-layout.tsx
@@ -1,72 +1,62 @@
+/* eslint-disable max-len */
+// https://oauth.yandex.ru/authorize?response_type=code&client_id=c709762dfe3e447999beb343da0bee9f
 import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+import { useGetYaTokenMutation, useSignInWitOauthYaMutation } from '../../store/api';
 
 import { CLIENT_ID, CLIENT_SECRET } from '../../utils/dev-config';
-import { BASE_API } from '../../utils';
-// https://oauth.yandex.ru/authorize?response_type=code&client_id=c709762dfe3e447999beb343da0bee9f
+import { Urls } from '../../utils';
+
+import style from './oauth-layout.module.css';
 
 export default function Oauth() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
-  const code: string = searchParams.get('code')!;
+  const [getYaToken] = useGetYaTokenMutation();
+  const [signInWitOauthYa] = useSignInWitOauthYaMutation();
+
+  const code = searchParams.get('code')!;
   const [token, setToken] = useState<string | null>(null);
   const [user, setUser] = useState<Record<string, string> | null>(null);
 
   useEffect(() => {
-    const myHeaders = new Headers();
-    const formdata = new FormData();
-    formdata.append('grant_type', 'authorization_code');
-    formdata.append('client_id', CLIENT_ID);
-    formdata.append('client_secret', CLIENT_SECRET);
-    formdata.append('code', code);
+    const getToken = async () => {
+      const body = new FormData();
+      body.append('grant_type', 'authorization_code');
+      body.append('code', code);
+      body.append('client_id', CLIENT_ID);
+      body.append('client_secret', CLIENT_SECRET);
 
-    const requestOptions: RequestInit = {
-      method: 'POST',
-      headers: myHeaders,
-      body: formdata,
-      redirect: 'follow',
+      const { data }: any = await getYaToken({ body });
+
+      if (data?.access_token) {
+        setToken(data.access_token);
+      }
     };
 
-    fetch('https://oauth.yandex.ru/token', requestOptions)
-      .then((response) => {
-        console.log(response);
-        return response.text();
-      })
-      .then((result) => {
-        setToken(JSON.parse(result).access_token);
-      })
-      .catch((error) => console.log('error', error));
+    getToken();
   }, [code]);
 
   useEffect(() => {
-    if (token) {
-      const raw = JSON.stringify({ token });
-      const myHeaders = new Headers();
-      myHeaders.append('Content-Type', 'application/json');
-      const requestOptions: RequestInit = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow',
-      };
+    const signIn = async () => {
+      if (token) {
+        const { data }: any = await signInWitOauthYa({ token });
 
-      fetch(`${BASE_API}/api/oauth`, requestOptions)
-        .then((response) => response.json())
-        .then((result: unknown) => {
-          setUser(result as Record<string, string>);
-          console.log(result);
-        })
-        .catch((error) => console.log('error', error));
-    }
+        if (data) {
+          setUser(data);
+          setTimeout(() => navigate(Urls.BASE.INDEX), 3000);
+        }
+      }
+    };
+
+    signIn();
   }, [token]);
 
   return (
-    <section className="page">
-      <div className="page__content">
-        {code}
-        -
-        {token}
-        {user?.default_email}
-        {user?.login}
+    <section className={style.layout}>
+      <div className={style.container}>
+        <h2 className={style.title}>Welcome!</h2>
       </div>
     </section>
   );
