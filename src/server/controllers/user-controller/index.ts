c073928437d48517b3bc3974cce5a299975ecf76
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-13 11:08:43 +0300

    fix: user-controller

--- src/server/controllers/user-controller/index.ts
+++ src/server/controllers/user-controller/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable consistent-return */
 import { NextFunction, Request, Response } from 'express';
 
-import { user } from '../../mocks/db';
+import { users } from '../../mocks/db';
 
 const addUser = (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -13,6 +13,9 @@ const addUser = (req: Request, res: Response, next: NextFunction) => {
 
 const getUserMe = (req: Request, res: Response, next: NextFunction) => {
   try {
+    const id = 0;
+    const user = users[id];
+
     return res.send(user);
   } catch (err) {
     next(err);
@@ -20,17 +23,12 @@ const getUserMe = (req: Request, res: Response, next: NextFunction) => {
 };
 
 const updateUser = (req: Request, res: Response, next: NextFunction) => {
-  const {
-    id, name, email, active, paid, project,
-  } = req.body;
-
-  user.name = name;
-  user.email = email;
-  user.active = active;
-  user.paid = paid;
-  user.project = project;
-
+  const { id, defaultEmail, project } = req.body;
   try {
+    const user = users.find((u) => u.defaultEmail === defaultEmail)!;
+    user.defaultEmail = defaultEmail;
+    user.project = project;
+
     return res.send(user);
   } catch (err) {
     next(err);
