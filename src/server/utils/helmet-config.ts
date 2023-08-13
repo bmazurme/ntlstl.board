b2cb5a8a5c95ed6e7cdbae0a4d0d2393419f0f3f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-13 11:04:48 +0300

    fix: helmet-config

--- src/server/utils/helmet-config.ts
+++ src/server/utils/helmet-config.ts
@@ -1,10 +1,10 @@
 const helmetConfig = {
   useDefaults: true,
   directives: {
-    defaultSrc: ["'self'", 'http://localhost:3000', 'https://oauth.yandex.ru/token'],
-    scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'http://localhost:3000', 'https://oauth.yandex.ru/token'],
-    connectSrc: ["'self'", 'http://localhost:3000', 'https://oauth.yandex.ru/token'],
-    styleSrc: ["'self'", "'unsafe-inline'", 'http://localhost:3000', 'https://oauth.yandex.ru/token'],
+    defaultSrc: ["'self'", 'http://localhost:3000', 'http://localhost:3001', 'https://oauth.yandex.ru/token'],
+    scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'http://localhost:3000', 'http://localhost:3001', 'https://oauth.yandex.ru/token'],
+    connectSrc: ["'self'", 'http://localhost:3000', 'http://localhost:3001', 'https://oauth.yandex.ru/token'],
+    styleSrc: ["'self'", "'unsafe-inline'", 'http://localhost:3000', 'http://localhost:3001', 'https://oauth.yandex.ru/token'],
     imgSrc: ["'self'", 'http://localhost:3000', 'https://oauth.yandex.ru/token'],
   },
 };
