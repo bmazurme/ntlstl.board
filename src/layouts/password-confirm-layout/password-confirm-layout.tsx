113d0b8813b2ba9adaf1e63c869e6831ba974bdb
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-14 08:14:18 +0300

    fix: password-confirm-layout

--- src/layouts/password-confirm-layout/password-confirm-layout.tsx
+++ src/layouts/password-confirm-layout/password-confirm-layout.tsx
@@ -3,7 +3,5 @@ import React from 'react';
 import style from './password-confirm-layout.module.css';
 
 export default function PasswordConfirmLayout() {
-  return (
-    <div>PasswordConfirm</div>
-  );
+  return (<div>PasswordConfirm</div>);
 }
