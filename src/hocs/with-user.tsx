d9fad6245ec46ec342b1f2893837adec46f1aec3
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-14 08:18:30 +0300

    fix: with-user

--- src/hocs/with-user.tsx
+++ src/hocs/with-user.tsx
@@ -1,19 +1,50 @@
-import React, { type ComponentType } from 'react';
+import React, { type ComponentType, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 
-import { useGetUserMeQuery } from '../store/api';
+import Preloader from '../components/preloader';
+
+import { useGetIamMutation } from '../store/api';
+import useUser from '../hooks/use-user';
+import { Urls } from '../utils';
 
 export default function withUser<P extends Record<string, unknown>>(
   Page: ComponentType<P>,
   shouldBeAuthorized = false,
 ) {
-  return function WithUser(pageProps: P) {
-    const { data: user } = useGetUserMeQuery() as { data: TypeUser };
-    const pagePropsWithUser = { ...pageProps, user };
+  return function WithUser(pageProps: P & { user?: TypeUser }) {
+    let user: TypeUser | null = useUser();
 
-    if (shouldBeAuthorized) {
+    const [getUser, {
+      isUninitialized,
+      isLoading,
+      isError,
+      error,
+      data,
+    }] = useGetIamMutation();
+
+    useEffect(() => {
+      if (isUninitialized && !user) {
+        getUser();
+
+        if (data && !isError) {
+          user = data;
+        }
+      }
+    }, [getUser, isError, isLoading, isUninitialized, user, data]);
+
+    if (isLoading || (isUninitialized && !user)) {
+      return <Preloader />;
+    }
+
+    if (!shouldBeAuthorized || user) {
+      const pagePropsWithUser = { ...pageProps, user };
+      return <Page {...pagePropsWithUser} />;
+    }
+
+    if (isError && (error as any).response?.status !== 401 && !shouldBeAuthorized) {
       return <div>Something went wrong</div>;
     }
 
-    return <Page {...pagePropsWithUser} />;
+    return <Navigate to={Urls.SIGN.IN} />;
   };
 }
