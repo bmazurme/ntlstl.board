d78cc42d2941c1bb285cb86b9026de8dddbaecef
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-14 08:13:57 +0300

    fix: password-forgot-layout

--- src/layouts/password-forgot-layout/password-forgot-layout.tsx
+++ src/layouts/password-forgot-layout/password-forgot-layout.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import InputField from '../../components/input-field';
 import Button from '../../components/button';
