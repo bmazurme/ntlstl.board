8e9a32e3600761cd877c09681f19eecba2c0fc58
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-17 08:15:49 +0300

    fix: auth-route

--- src/server/routes/auth-route/index.ts
+++ src/server/routes/auth-route/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 import { logout } from '../../controllers/auth-controller';
 
+// import { UrlsApi } from '../../utils/routers';
+
 const router = Router();
 
 router.post('/logout', logout);
