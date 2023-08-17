7c691785ec1f75fac4d59d3f5c485e7f54a581a0
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-17 08:13:22 +0300

    fix: user-route

--- src/server/routes/user-route/index.ts
+++ src/server/routes/user-route/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 
-import {
-  getUserMe, updateUser, addUser,
-} from '../../controllers';
+import { getUserMe, updateUser } from '../../controllers';
+
+// import { UrlsApi } from '../../utils/routers';
 
 const router = Router();
 
-router.post('/user', addUser);
+// router.post('/user', addUser);
 router.get('/user', getUserMe);
 router.patch('/user', updateUser);
 
