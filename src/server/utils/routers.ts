5cfd6c8c73701f8f687b158a4663d3cfad911a45
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-17 08:11:09 +0300

    fix: router

---
+++ src/server/utils/routers.ts
@@ -0,0 +1,25 @@
+export const UrlsApi = {
+  SIGN: {
+    OAUTH: '/oauth',
+    OUT: '/logout',
+  },
+  USER: {
+    ME: '/users/me',
+  },
+  PROJECT: {
+    INDEX: '/projects',
+    DELETE: '/projects/:id',
+  },
+  BOOK: {
+    INDEX: '/books',
+    DELETE: '/books/:id',
+  },
+  BLOCK: {
+    INDEX: '/blocks',
+    DELETE: '/blocks/:id',
+  },
+  ITEM: {
+    INDEX: '/items',
+    DELETE: '/items/:id',
+  },
+};
