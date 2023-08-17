7d625cb6523b8c7fa79c628b350fc85f4a7a4f93
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-17 08:16:56 +0300

    fix: user-controller

---
+++ src/server/controllers/user-controller/user-controller.ts
@@ -0,0 +1,42 @@
+/* eslint-disable max-len */
+/* eslint-disable consistent-return */
+import { NextFunction, Request, Response } from 'express';
+
+import NotFoundError from '../../errors/not-found-error';
+
+import Users from '../../models/user-model';
+
+const getUserMe = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // @ts-ignore
+    const user = await Users.findOne({ defaultEmail: req.user.default_email });
+
+    if (!user) {
+      return next(new NotFoundError('User not found'));
+    }
+
+    return res.send({ defaultEmail: user.defaultEmail, projectId: user.projectId });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // @ts-ignore
+    const { default_email } = req.user;
+    const { projectId } = req.body;
+    const options = { new: true };
+    const user = await Users.findOneAndUpdate({ defaultEmail: default_email }, { projectId }, options);
+
+    if (!user) {
+      return next(new NotFoundError('User not found'));
+    }
+
+    return res.send({ defaultEmail: user.defaultEmail, projectId: user.projectId });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export { getUserMe, updateUser };
