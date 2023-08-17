456a349b2dfae8e658bd1c748689783bd9a7106c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-17 08:16:03 +0300

    fix: block-route

--- src/server/routes/block-route/index.ts
+++ src/server/routes/block-route/index.ts
@@ -3,17 +3,19 @@ import { Router } from 'express';
 import {
   getBlocks,
   updateBlocks,
-  removeBlock,
+  deleteBlock,
   addBlock,
   renameBlock,
   setMovedBlock,
 } from '../../controllers/blocks-controller';
 
+import { UrlsApi } from '../../utils/routers';
+
 const router = Router();
 
 router.get('/blocks/:id', getBlocks);
 router.patch('/blocks', updateBlocks);
-router.put('/blocks/book', removeBlock);
+router.put('/blocks/book', deleteBlock);
 router.post('/blocks', addBlock);
 router.patch('/blocks/rename', renameBlock);
 router.patch('/blocks/set-moved-block', setMovedBlock);
