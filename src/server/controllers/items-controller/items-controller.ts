2ed4d86ab63beb6f0827c2ad97e56d36d746d734
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-18 08:13:19 +0300

    fix: items-controller

--- src/server/controllers/items-controller/items-controller.ts
+++ src/server/controllers/items-controller/items-controller.ts
@@ -42,18 +42,30 @@ dotEnvConfig();
 const addItem = async (req: any, res: Response, next: NextFunction) => {
   try {
     const { bookId, index, blockId } = req.body;
-    await Items.create({
-      index, blockId, bookId, result: 0,
+    const items = await Items.find({ bookId });
+    const item = await Items.create({
+      index: items.length, blockId, bookId, result: 0,
     });
+    items.push(item);
 
     const blocks = await Blocks.find({ bookId });
-    const items = await Items.find({ bookId });
+
     const data = blocks.reduce((a, x, i) => ({
       ...a,
       [i]: {
+        blockId: x._id,
         index: x.index,
         name: x.name,
-        items: items.filter((it) => it.blockId === x._id),
+        items: items.filter((it) => it.blockId.equals(x._id))
+          .map((it) => ({
+            id: it._id,
+            item: { value: '0', label: it._id },
+            values: [],
+            blockId: it.blockId,
+            bookId: it.blockId,
+            index: it.index,
+            result: it.result,
+          })),
       },
     }), {});
 
@@ -94,16 +106,57 @@ const updateItem = async (req: any, res: Response, next: NextFunction) => {
   }
 };
 
+// const removeItem = (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const { id, block, blockId } = req.body;
+//     const item = blocks[blockId].value;
+//     blocks[blockId].value = {
+//       ...item,
+//       [block]: {
+//         ...item[block],
+//         items: item[block].items.filter((x) => x.id !== id),
+//       },
+//     };
+
+//     return res.send(blocks[blockId].value);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
 const deleteItem = async (req: any, res: Response, next: NextFunction) => {
   try {
-    const { id } = req.params;
-    const data = await Items.findByIdAndDelete(id);
+    // const { id } = req.params;
+    const { id, bookId } = req.body;
+    const item = await Items.findByIdAndDelete(id);
 
-    if (!data) {
+    if (!item) {
       return next(new NotFoundError('Item not found'));
     }
 
-    return res.send({ message: `Item with id '${id}' has been deleted.` });
+    const blocks = await Blocks.find({ bookId });
+    const items = await Items.find({ bookId });
+
+    const data = blocks.reduce((a, x, i) => ({
+      ...a,
+      [i]: {
+        blockId: x._id,
+        index: x.index,
+        name: x.name,
+        items: items.filter((it) => it.blockId.equals(x._id))
+          .map((it) => ({
+            id: it._id,
+            item: { value: '0', label: 'item' },
+            values: [],
+            blockId: it.blockId,
+            bookId: it.blockId,
+            index: it.index,
+            result: it.result,
+          })),
+      },
+    }), {});
+
+    return res.send(data);
   } catch (err) {
     return next(err);
   }
