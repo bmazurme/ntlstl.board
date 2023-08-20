dc10cb79ae6865a35edbf04d15bd0b0f42e7671e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-20 18:07:48 +0300

    fix: routes

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import authRoute from './auth-route';
 import blocksRoute from './blocks-route';
 import itemsRoute from './items-route';
+import itemTypesRoute from './item-types-route';
 import bookRoute from './book-route';
 import oauthRoute from './oauth-route';
 import projectRoute from './project-route';
 import notificationRoute from './notification-route';
 import userRoute from './user-route';
 import workplaceRoute from './workplace-route';
+import fieldsRoute from './fields-route';
 
 import authMiddleware from '../middlewares/auth';
 
@@ -16,6 +18,7 @@ const router = Router();
 
 router.use('/', oauthRoute);
 router.use('/', authRoute);
+router.use('/', itemTypesRoute);
 router.use('/', authMiddleware, blocksRoute);
 router.use('/', authMiddleware, itemsRoute);
 router.use('/', authMiddleware, bookRoute);
@@ -23,5 +26,6 @@ router.use('/', authMiddleware, projectRoute);
 router.use('/', authMiddleware, notificationRoute);
 router.use('/', authMiddleware, userRoute);
 router.use('/', authMiddleware, workplaceRoute);
+router.use('/', authMiddleware, fieldsRoute);
 
 export default router;
