61c30fda4909c920b32e26a1fbe31671147a1c27
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-20 18:08:57 +0300

    fix: socket

--- src/server/socket/index.ts
+++ src/server/socket/index.ts
@@ -1,6 +1,14 @@
 import WebSocket from 'ws';
 import { mock } from '../mocks/mock';
 
+// import Items from './models/item-model';
+
+// const collection = db.collection('Books');
+// const changeStream = Fields.watch();
+// changeStream.on('change', event => {
+//   // event — см. типы событий: https://docs.mongodb.com/manual/reference/change-events/
+// });
+
 const onConnection = (ws: WebSocket) => {
   // connection is up, let's add a simple simple event
   ws.on('message', (message: string) => {
@@ -8,6 +16,12 @@ const onConnection = (ws: WebSocket) => {
     console.log('received: %s', message);
     ws.send(`Hello, you sent -> ${message}`);
   });
+
+  // const itemsStream = Items.watch();
+  // itemsStream.on('delete', (event) => {
+  // // event — см. типы событий: https://docs.mongodb.com/manual/reference/change-events/
+  //   console.log(event);
+  // });
 
   ws.send(JSON.stringify(mock));
 };
