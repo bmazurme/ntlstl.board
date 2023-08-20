ac86875d53cd7ff95f23135cc4c989f3b68f1386
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-20 18:08:28 +0300

    fix: items-route

--- src/server/routes/items-route/index.ts
+++ src/server/routes/items-route/index.ts
@@ -3,21 +3,20 @@ import { Router } from 'express';
 import {
   deleteItem,
   setMovedItem,
-  changeItemValues,
   changeItemValue,
   getItemResult,
   addItem,
 } from '../../controllers/items-controller';
 
-// import { UrlsApi } from '../../utils/routers';
+import { UrlsApi } from '../../utils/routers';
 
 const router = Router();
 
+router.post(UrlsApi.ITEMS.INDEX, addItem);
+router.patch(UrlsApi.ITEMS.INDEX, changeItemValue);
+router.put(UrlsApi.ITEMS.INDEX, setMovedItem);
 router.delete('/items', deleteItem);
-router.patch('/items', setMovedItem);
-router.patch('/items/values', changeItemValues);
-router.patch('/items/value', changeItemValue);
+
 router.patch('/items/result', getItemResult);
-router.post('/items', addItem);
 
 export default router;
