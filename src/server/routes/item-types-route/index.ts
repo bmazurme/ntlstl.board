1b3af15608c4d344e35fe4bf104c5ba0e392550c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-21 08:29:45 +0300

    fix: item-types-route

--- src/server/routes/item-types-route/index.ts
+++ src/server/routes/item-types-route/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 
 import { addItemType, getItemTypes } from '../../controllers/item-types-controller';
 
+import { UrlsApi } from '../../utils/routers';
+
 const router = Router();
 
-router.post('/item-types', addItemType);
-router.get('/item-types', getItemTypes);
+router.post(UrlsApi.ITEM_TYPES.INDEX, addItemType);
+router.get(UrlsApi.ITEM_TYPES.INDEX, getItemTypes);
 
 export default router;
