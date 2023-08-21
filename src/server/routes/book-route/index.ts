281cdc4099a4118d36013f5371e456314734dd2f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-21 08:30:21 +0300

    fix: book-route

--- src/server/routes/book-route/index.ts
+++ src/server/routes/book-route/index.ts
@@ -3,12 +3,12 @@ import { Router } from 'express';
 import { getBooks, addBook } from '../../controllers';
 import { renameBook } from '../../controllers/books-controller';
 
-// import { UrlsApi } from '../../utils/routers';
+import { UrlsApi } from '../../utils/routers';
 
 const router = Router();
 
-router.get('/books/:id', getBooks);
-router.post('/books', addBook);
-router.patch('/books', renameBook);
+router.get(UrlsApi.BOOKS.ID, getBooks);
+router.post(UrlsApi.BOOKS.INDEX, addBook);
+router.patch(UrlsApi.BOOKS.INDEX, renameBook);
 
 export default router;
