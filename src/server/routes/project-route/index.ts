d0ae50f5d86c7620647cdeca54577519a1f50a4a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-21 08:29:23 +0300

    fix: project-route

--- src/server/routes/project-route/index.ts
+++ src/server/routes/project-route/index.ts
@@ -2,12 +2,12 @@ import { Router } from 'express';
 
 import { getProjects, addProject, updateProject } from '../../controllers/projects-controller';
 
-// import { UrlsApi } from '../../utils/routers';
+import { UrlsApi } from '../../utils/routers';
 
 const router = Router();
 
-router.get('/projects', getProjects);
-router.post('/projects', addProject);
-router.patch('/projects', updateProject);
+router.get(UrlsApi.PROJECT.INDEX, getProjects);
+router.post(UrlsApi.PROJECT.INDEX, addProject);
+router.patch(UrlsApi.PROJECT.INDEX, updateProject);
 
 export default router;
