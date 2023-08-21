fac40b691ceccf3f8c81881281b7e1c9c3738291
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-21 08:31:51 +0300

    fix: items-controller

--- src/server/controllers/items-controller/items-controller.ts
+++ src/server/controllers/items-controller/items-controller.ts
@@ -1,73 +1,73 @@
+/* eslint-disable max-len */
 /* eslint-disable consistent-return */
 /* eslint-disable no-param-reassign */
-import { NextFunction, Response } from 'express';
+import { NextFunction, Response, Request } from 'express';
 import { config as dotEnvConfig } from 'dotenv';
 
+import { Types } from 'mongoose';
 import NotFoundError from '../../errors/not-found-error';
 
-import Blocks from '../../models/block-model';
-import Items from '../../models/item-model';
+import Blocks, { IBlock } from '../../models/block-model';
+import Items, { IItem } from '../../models/item-model';
+import ItemTypes from '../../models/item-type-model';
+import Fields, { IField } from '../../models/field-model';
 
 dotEnvConfig();
 
-// const addItem = (req: Request, res: Response, next: NextFunction) => {
-//   try {
-//     const { bookId, index } = req.body;
-//     const currentItem = blocks[bookId].value[index];
-//     blocks[bookId].value = {
-//       ...blocks[bookId].value,
-//       [index]: {
-//         ...currentItem,
-//         items: [
-//           ...currentItem.items,
-//           {
-//             id: uuidv4(),
-//             item: {
-//               value: `${currentItem.items.length}`,
-//               label: `Item ${currentItem.items.length + 1}`,
-//             },
-//             values: data,
-//             result: 0,
-//           },
-//         ],
-//       },
-//     };
+const getData = async (blocks: IBlock[], items: IItem[], fields: IField[]) => {
+  const itemTypes = await ItemTypes.find({});
+  const getItem = (it: IItem) => {
+    if (!it.itemType) {
+      return { value: 'null', label: '-' };
+    }
 
-//     return res.send(blocks[bookId].value);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
+    const type = itemTypes.find((t) => it.itemType.equals(t._id));
+    return { value: type?._id, label: type?.name };
+  };
 
-const addItem = async (req: any, res: Response, next: NextFunction) => {
+  return blocks
+    .sort((a, b) => a.index - b.index)
+    .reduce((a, x, i) => ({
+      ...a,
+      [i]: {
+        blockId: x._id,
+        index: x.index,
+        name: x.name,
+        items: items
+          .filter((it) => it.blockId.equals(x._id))
+          .map((it) => ({
+            id: it._id,
+            item: it.itemType ? getItem(it) : null,
+            values: fields.filter((field) => field.itemId.equals(it._id)),
+            blockId: it.blockId,
+            bookId: it.blockId,
+            index: it.index,
+            result: it.result,
+          }))
+          .sort((b, c) => b.index - c.index),
+      },
+    }), {});
+};
+
+const addItem = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { bookId, index, blockId } = req.body;
     const items = await Items.find({ bookId });
     const item = await Items.create({
       index: items.length, blockId, bookId, result: 0,
     });
+
+    const bookSet = [1, 2, 3];
+    const bookFields = bookSet.map((x) => ({
+      bookId, itemId: item._id, value: 0, name: `field${x}`,
+    }));
+
+    await Fields.insertMany(bookFields);
     items.push(item);
 
     const blocks = await Blocks.find({ bookId });
-
-    const data = blocks.reduce((a, x, i) => ({
-      ...a,
-      [i]: {
-        blockId: x._id,
-        index: x.index,
-        name: x.name,
-        items: items.filter((it) => it.blockId.equals(x._id))
-          .map((it) => ({
-            id: it._id,
-            item: { value: '0', label: it._id },
-            values: [],
-            blockId: it.blockId,
-            bookId: it.blockId,
-            index: it.index,
-            result: it.result,
-          })),
-      },
-    }), {});
+    const fields = await Fields.find({ bookId });
+    const data = await getData(blocks, items, fields);
 
     return res.status(200).send(data);
   } catch (err) {
@@ -75,7 +75,7 @@ const addItem = async (req: any, res: Response, next: NextFunction) => {
   }
 };
 
-const getItems = async (req: any, res: Response, next: NextFunction) => {
+const getItems = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const item = req.body;
     const data = await Items.find({ blockId: item.blockId });
@@ -90,7 +90,7 @@ const getItems = async (req: any, res: Response, next: NextFunction) => {
   }
 };
 
-const updateItem = async (req: any, res: Response, next: NextFunction) => {
+const updateItem = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id, ...newData } = req.body;
     const options = { new: true };
@@ -106,55 +106,31 @@ const updateItem = async (req: any, res: Response, next: NextFunction) => {
   }
 };
 
-// const removeItem = (req: Request, res: Response, next: NextFunction) => {
-//   try {
-//     const { id, block, blockId } = req.body;
-//     const item = blocks[blockId].value;
-//     blocks[blockId].value = {
-//       ...item,
-//       [block]: {
-//         ...item[block],
-//         items: item[block].items.filter((x) => x.id !== id),
-//       },
-//     };
-
-//     return res.send(blocks[blockId].value);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-
-const deleteItem = async (req: any, res: Response, next: NextFunction) => {
+const deleteItem = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    // const { id } = req.params;
-    const { id, bookId } = req.body;
-    const item = await Items.findByIdAndDelete(id);
+    const { id: itemId, bookId } = req.body;
+    const item = await Items.findByIdAndDelete(itemId);
 
     if (!item) {
       return next(new NotFoundError('Item not found'));
     }
 
+    await Fields.deleteMany({ itemId });
+
+    const itemsByBlock = await Items.find({ blockId: item.blockId });
+    const itemsBulk = Items.collection.initializeOrderedBulkOp();
+    itemsByBlock.sort((a, b) => a.index - b.index).forEach(({ _id }, i) => {
+      itemsBulk.find({ _id }).update({ $set: { index: i } });
+    });
+
+    if (itemsBulk.batches.length > 0) {
+      itemsBulk.execute();
+    }
+
     const blocks = await Blocks.find({ bookId });
     const items = await Items.find({ bookId });
-
-    const data = blocks.reduce((a, x, i) => ({
-      ...a,
-      [i]: {
-        blockId: x._id,
-        index: x.index,
-        name: x.name,
-        items: items.filter((it) => it.blockId.equals(x._id))
-          .map((it) => ({
-            id: it._id,
-            item: { value: '0', label: 'item' },
-            values: [],
-            blockId: it.blockId,
-            bookId: it.blockId,
-            index: it.index,
-            result: it.result,
-          })),
-      },
-    }), {});
+    const fields = await Fields.find({ bookId });
+    const data = await getData(blocks, items, fields);
 
     return res.send(data);
   } catch (err) {
@@ -162,6 +138,46 @@ const deleteItem = async (req: any, res: Response, next: NextFunction) => {
   }
 };
 
+const setMovedItem = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { bookId, data: init } = req.body;
+    const itemsBulk = Items.collection.initializeOrderedBulkOp();
+
+    Object.keys(init).forEach((key) => {
+      init[key].items.forEach(({ id, index }: IItem) => {
+        itemsBulk.find({ _id: new Types.ObjectId(id) }).update({ $set: { index } });
+      });
+    });
+
+    itemsBulk.execute();
+
+    const blocks = await Blocks.find({ bookId });
+    const items = await Items.find({ bookId });
+    const fields = await Fields.find({ bookId });
+    const data = await getData(blocks, items, fields);
+
+    return res.send(data);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const changeItemValue = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { itemId, itemType, bookId } = req.body;
+
+    await Items.findByIdAndUpdate({ _id: itemId }, { itemType });
+    const blocks = await Blocks.find({ bookId });
+    const items = await Items.find({ bookId });
+    const fields = await Fields.find({ bookId });
+    const data = await getData(blocks, items, fields);
+
+    return res.send(data);
+  } catch (err) {
+    next(err);
+  }
+};
+
 export {
-  addItem, getItems, updateItem, deleteItem,
+  addItem, getItems, updateItem, deleteItem, setMovedItem, changeItemValue,
 };
