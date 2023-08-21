fe6dfaecd59919ad89123d12cd515b036380f958
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-21 08:34:45 +0300

    fix: mocks

--- src/mocks/items.ts
+++
@@ -1,8 +0,0 @@
-const items = [
-  { value: '0', label: 'Item 1' },
-  { value: '1', label: 'Item 2' },
-  { value: '2', label: 'Item 3' },
-  { value: '3', label: 'Item 4' },
-];
-
-export { items };
