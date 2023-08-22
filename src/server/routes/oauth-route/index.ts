5930aa067619b0e1b7acc4f4512e41984366a27d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-22 08:13:28 +0300

    fix: oauth-route

--- src/server/routes/oauth-route/index.ts
+++ src/server/routes/oauth-route/index.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
-import oauthYaSigninController from '../../controllers/oauth-controller';
+import { oauthYaSigninController, oauthGithubSigninController } from '../../controllers/oauth-controller';
 
 import { UrlsApi } from '../../utils/routers';
 
 const router = Router();
 
 router.post(UrlsApi.SIGN.OAUTH, oauthYaSigninController);
+router.post('/oauth/github', oauthGithubSigninController);
 
 export default router;
