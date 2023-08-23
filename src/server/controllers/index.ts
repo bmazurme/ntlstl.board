129bd3a36c176588804085033cdf9b3921d35fe8
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-23 08:19:23 +0300

    fix: controllers

--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,16 +1,6 @@
 import { getBooks, addBook } from './books-controller';
 import { getUserMe, updateUser } from './user-controller';
-import {
-  getWorkplace, renameWorkplace, updateWorkplace, moveToArchive,
-} from './workplace-controller';
 
 export {
-  getUserMe,
-  getWorkplace,
-  renameWorkplace,
-  updateWorkplace,
-  moveToArchive,
-  updateUser,
-  getBooks,
-  addBook,
+  getUserMe, updateUser, getBooks, addBook,
 };
