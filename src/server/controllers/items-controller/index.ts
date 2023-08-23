42981ab9b17da4b3d8d46a53af0a3aec07ddeea4
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-23 08:19:11 +0300

    fix: items-controller

--- src/server/controllers/items-controller/index.ts
+++ src/server/controllers/items-controller/index.ts
@@ -38,7 +38,7 @@ const getItemResult = (req: Request, res: Response, next: NextFunction) => {
         items: [...blocks[bookId].value[index].items]
           .map((x) => (x.id === id ? {
             ...x,
-            result: x.values.reduce((a: number, i: { value: number }) => a + Number(i.value), 0),
+            result: x.values.reduce((a: number, i: { value: string }) => a + Number(i.value), 0),
           } : x)),
       },
     };
