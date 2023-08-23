3e36582134fdfe0ce2c9d36b2c67ba68305245f2
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-23 08:15:16 +0300

    fix: routes

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -9,7 +9,6 @@ import oauthRoute from './oauth-route';
 import projectRoute from './project-route';
 import notificationRoute from './notification-route';
 import userRoute from './user-route';
-import workplaceRoute from './workplace-route';
 import fieldsRoute from './fields-route';
 
 import authMiddleware from '../middlewares/auth';
@@ -25,7 +24,6 @@ router.use('/', authMiddleware, bookRoute);
 router.use('/', authMiddleware, projectRoute);
 router.use('/', authMiddleware, notificationRoute);
 router.use('/', authMiddleware, userRoute);
-router.use('/', authMiddleware, workplaceRoute);
 router.use('/', authMiddleware, fieldsRoute);
 
 export default router;
