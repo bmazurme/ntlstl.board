11a99213b8d57a120f60afd4d13b8de95b16915b
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-24 08:13:29 +0300

    fix: mocks

--- src/server/mocks/db.ts
+++ src/server/mocks/db.ts
@@ -1,43 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
-
-import { values } from '../../mocks/values';
-
-const blocks: { id: string; value: TypeBlock }[] = [
-  {
-    id: '0',
-    value: {
-      0: {
-        index: 0,
-        name: 'BLOCK1',
-        items: [
-          {
-            id: uuidv4(), item: { value: '0', label: 'Item 1' }, values, result: 0,
-          },
-          {
-            id: uuidv4(), item: { value: '1', label: 'Item 2' }, values, result: 0,
-          },
-          {
-            id: uuidv4(), item: { value: '2', label: 'Item 3' }, values, result: 0,
-          },
-          {
-            id: uuidv4(), item: { value: '3', label: 'Item 4' }, values, result: 0,
-          },
-          {
-            id: uuidv4(), item: { value: '4', label: 'Item 5' }, values, result: 0,
-          },
-        ],
-        blockId: '0',
-      },
-      1: {
-        index: 1,
-        name: 'BLOCK2',
-        items: [],
-        blockId: '1',
-      },
-    },
-  },
-];
-
 const mock = [{
   id: 0,
   title: 'title',
@@ -57,5 +17,5 @@ type TypeProject = { userId: string; items: { value: string; label: string; }[];
 const projects: TypeProject[] = [{ userId: '0', items: [{ value: '0', label: 'Project 1' }] }];
 
 export {
-  blocks, mock, users, projects,
+  mock, users, projects,
 };
