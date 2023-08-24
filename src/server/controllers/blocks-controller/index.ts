519e97c90e4b055436f8268c7046771b98b1b820
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-24 08:14:16 +0300

    fix: blocks-controller

--- src/server/controllers/blocks-controller/index.ts
+++ src/server/controllers/blocks-controller/index.ts
@@ -1,32 +1,6 @@
-/* eslint-disable no-return-assign */
-/* eslint-disable consistent-return */
-import { NextFunction, Request, Response } from 'express';
-
 import {
   getBlocks, addBlock, deleteBlock, renameBlock, setBlocks, updateBlocks,
 } from './blocks-controller';
-
-import { blocks } from '../../mocks/db';
-
-const setMovedBlock = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const {
-      bookId, dragIndex, hoverIndex, //  item, // !!!
-    } = req.body;
-
-    const data = blocks[bookId].value;
-    const obj: TypeBlock = {};
-    const coppiedStateArray = [...Object.keys(data)].map((x) => Number(x));
-    coppiedStateArray.splice(hoverIndex, 1, dragIndex);
-    coppiedStateArray.splice(dragIndex, 1, hoverIndex);
-    coppiedStateArray.forEach((x, i: number) => obj[i] = { ...data[x], index: i });
-    blocks[bookId].value = obj;
-
-    return res.send(blocks[bookId].value);
-  } catch (err) {
-    next(err);
-  }
-};
 
 export {
   getBlocks,
@@ -34,6 +8,5 @@ export {
   deleteBlock,
   addBlock,
   renameBlock,
-  setMovedBlock,
   setBlocks,
 };
