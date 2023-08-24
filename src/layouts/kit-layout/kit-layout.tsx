6d373c8b1f8ac5d0bc0ceecec3a690fc38ccd04a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-24 08:15:53 +0300

    fix: kit-layout

---
+++ src/layouts/kit-layout/kit-layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Kit from '../../components/kit';
+
+import style from './kit-layout.module.css';
+
+export default function KitLayout() {
+  return (<Kit />);
+}
