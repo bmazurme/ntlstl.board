fc4ced13425425f8b007a9b08a0e21bc30d4422f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-24 08:16:32 +0300

    fix: input-field

--- src/components/input-field/input-field.tsx
+++ src/components/input-field/input-field.tsx
@@ -2,14 +2,30 @@ import React from 'react';
 
 import style from './input-field.module.css';
 
-export default function InputField({ type, name, errors }
-  : { type: 'text' | 'password', name: string, errors: Record<string, string> }) {
+export default function InputField({
+  type, name, errors, onChange, value,
+}: {
+  type: 'text' | 'password';
+  name: string;
+  value?: string;
+  errors: Record<string, string>;
+  onChange?: () => void;
+}) {
+
   return (
     <div className={style.inputfield}>
-      <input className={style.input} type={type} placeholder={name} />
-      <span className={style.error}>
-        {errors[name] && `${errors[name]}`}
-      </span>
+      <input
+        type={type}
+        className={style.input}
+        name={name}
+        value={value}
+        placeholder={name}
+        onChange={onChange}
+      />
+      {errors[name] &&
+        <span className={style.error}>
+          {`${errors[name]}`}
+        </span>}
     </div>
   );
 }
