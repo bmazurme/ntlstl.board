83fde1ff99f7cec52c2787a19da21386cf7ac0ac
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-28 08:19:42 +0300

    fix: get-rain-flow-from-roof

---
+++ src/server/calcs/rain-water/get-rain-flow-from-roof.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-mixed-operators */
+/* eslint-disable max-len */
+import { getQ5, getSummRoofArea } from '../utils';
+
+export default function getRainFlowFromRoof(areaRoof: number, areaFacade: number, n: number, q20: number, slope: number) {
+  const sumRoofArea = getSummRoofArea(areaRoof, areaFacade);
+
+  if (slope < 1.5) {
+    return {
+      flow: sumRoofArea * q20 / 10000,
+      sumRoofArea,
+    };
+  }
+  const q5 = getQ5(n, q20);
+  return {
+    flow: sumRoofArea * q5 / 10000,
+    sumRoofArea,
+    q5,
+  };
+}
