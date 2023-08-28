99b2b67411be062e28abdabf48d6b3b4bfb50297
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-28 08:20:09 +0300

    fix: oauth-github-layout

--- src/layouts/oauth-github-layout/oauth-github-layout.tsx
+++ src/layouts/oauth-github-layout/oauth-github-layout.tsx
@@ -2,6 +2,7 @@
 // https://oauth.yandex.ru/authorize?response_type=code&client_id=c709762dfe3e447999beb343da0bee9f
 import React, { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useErrorBoundary } from 'react-error-boundary';
 
 import { useSignInWitOauthGithubMutation } from '../../store/api';
 
@@ -12,15 +13,20 @@ import style from './oauth-github-layout.module.css';
 export default function Oauth() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
+  const { showBoundary } = useErrorBoundary();
   const [signInWitOauthGithub] = useSignInWitOauthGithubMutation();
   const code = searchParams.get('code')!;
 
   useEffect(() => {
     const getToken = async () => {
-      const { data }: any = await signInWitOauthGithub({ code });
+      try {
+        const { data }: any = await signInWitOauthGithub({ code });
 
-      if (data) {
-        setTimeout(() => navigate(Urls.BASE.INDEX), 1000);
+        if (data) {
+          setTimeout(() => navigate(Urls.BASE.INDEX), 1000);
+        }
+      } catch (error) {
+        showBoundary(error);
       }
     };
 
