986a95f5fada740dfdc860b61b3d2721881b8d65
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-06 09:43:20 +0300

    fix: support

--- src/components/support/index.tsx
+++ src/components/support/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import style from './support.module.css';
+// import style from './support.module.css';
 
 export default function Support() {
   return (<h2>Support</h2>);
