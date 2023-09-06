297df0ded7a9cba092be41fb5b83a529d3aff733
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-06 09:45:01 +0300

    fix: modal

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -13,12 +13,9 @@ type TypeModal = {
   title?: string;
   children: ReactNode;
   onClose: () => void;
-  isOpen: boolean;
 };
 
-export default function Modal({
-  title, children, onClose, isOpen,
-}: TypeModal) {
+export default function Modal({ title, children, onClose }: TypeModal) {
   const reactModals = document.getElementById('modals');
   const handleEscape = (e: KeyboardEvent) => {
     if (e.type === 'keydown' && e.code === 'Escape') {
@@ -41,23 +38,21 @@ export default function Modal({
   return ReactDOM.createPortal(
     <AnimatePresence>
       <ModalOverlay closeModal={closeModal}>
-        {isOpen && (
-          <motion.div
-            className={style.container}
-            onClick={(e) => e.stopPropagation()}
-            initial={MODAL_CONFIG.INITIAL}
-            animate={MODAL_CONFIG.ANIMATE}
-            exit={MODAL_CONFIG.EXIT}
-          >
-            {title && <h2 className="">{title}</h2>}
-            <button type="button" className={style.close} data-test="close-button">
-              <XMarkIcon className="h-6 w-6" onClick={onClose} />
-            </button>
-            <div className={style.wrapper}>
-              {children}
-            </div>
-          </motion.div>
-        )}
+        <motion.div
+          className={style.container}
+          onClick={(e) => e.stopPropagation()}
+          initial={MODAL_CONFIG.INITIAL}
+          animate={MODAL_CONFIG.ANIMATE}
+          exit={MODAL_CONFIG.EXIT}
+        >
+          {title && <h2 className="">{title}</h2>}
+          <button type="button" className={style.close} data-test="close-button">
+            <XMarkIcon className="h-6 w-6" onClick={onClose} />
+          </button>
+          <div className={style.wrapper}>
+            {children}
+          </div>
+        </motion.div>
       </ModalOverlay>
     </AnimatePresence>,
     reactModals!,
