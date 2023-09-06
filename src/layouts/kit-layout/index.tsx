8dd8fed8a2224e04641d0ada0ed52d05209ef9a8
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-06 09:42:29 +0300

    fix: kit-layout

--- src/layouts/kit-layout/index.tsx
+++ src/layouts/kit-layout/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Kit from '../../components/kit';
 
-import style from './kit-layout.module.css';
+// import style from './kit-layout.module.css';
 
 export default function KitLayout() {
   return (<Kit />);
