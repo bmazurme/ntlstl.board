189698109d623709bc5c952035f67afb7f37b8ba
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-06 09:44:37 +0300

    fix: icon-button

--- src/components/icon-button/index.tsx
+++ src/components/icon-button/index.tsx
@@ -4,11 +4,11 @@ import classNames from 'classnames';
 import style from './icon-button.module.css';
 
 type TypeIcon = React.ForwardRefExoticComponent<React.PropsWithoutRef<React.SVGProps<SVGSVGElement>>
-  & { title?: string, titleId?: string }
+  & { title?: string; titleId?: string; }
   & React.RefAttributes<SVGSVGElement>>;
 
 export default function IconButton({ handler, component: Component, disabled }
-  : { handler: () => void, component: TypeIcon, disabled?: boolean }) {
+  : { handler: () => void; component: TypeIcon; disabled?: boolean; }) {
   return (
     <button
       type="button"
