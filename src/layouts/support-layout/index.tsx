cc4d17c3b2e03cbf375c2ee65ca9fbf57a486f79
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-06 09:40:52 +0300

    fix: support-layout

--- src/layouts/support-layout/index.tsx
+++ src/layouts/support-layout/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Support from '../../components/support';
 import Container from '../../components/container';
 
-import style from './support-layout.module.css';
+// import style from './support-layout.module.css';
 
 export default function SupportLayout() {
   return (<Container header children={<Support />} />);
