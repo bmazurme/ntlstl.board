aa47293b29d60b6b7257b011931e8b6862478761
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-07 08:07:17 +0300

    fix: board

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/jsx-no-useless-fragment */
 import React from 'react';
 
-import Blocks from '../blocks';
-import WorkplaceHeader from '../workplace-header';
-import WorkplaceTools from '../workplace-tools';
+import Blocks from '../blocks/blocks';
+import WorkplaceHeader from '../workplace-header/workplace-header';
+import WorkplaceTools from '../workplace-tools/workplace-tools';
 
 import { useBook } from '../../hooks';
 
