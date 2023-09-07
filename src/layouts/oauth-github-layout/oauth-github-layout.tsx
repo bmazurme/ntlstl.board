e710c2753862c48c0802c3405f6e1dc335b4edf4
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-07 08:19:16 +0300

    fix: oauth-github-layout

--- src/layouts/oauth-github-layout/oauth-github-layout.tsx
+++ src/layouts/oauth-github-layout/oauth-github-layout.tsx
@@ -4,40 +4,36 @@ import React, { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useErrorBoundary } from 'react-error-boundary';
 
+import Preloader from '../../components/preloader/preloader';
+import WelcomeBlock from '../../components/welcome-block/welcome-block';
+
 import { useSignInWitOauthGithubMutation } from '../../store/api';
 
 import { Urls } from '../../utils';
 
-import style from './oauth-github-layout.module.css';
-
-export default function Oauth() {
+export default function OauthGithubLayout() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const { showBoundary } = useErrorBoundary();
-  const [signInWitOauthGithub] = useSignInWitOauthGithubMutation();
+  const [signInWitOauthGithub, { isError, isLoading, error }] = useSignInWitOauthGithubMutation();
   const code = searchParams.get('code')!;
 
   useEffect(() => {
     const getToken = async () => {
-      try {
-        const { data }: any = await signInWitOauthGithub({ code });
+      await signInWitOauthGithub({ code });
 
-        if (data) {
-          setTimeout(() => navigate(Urls.BASE.INDEX), 1000);
-        }
-      } catch (error) {
-        showBoundary(error);
+      if (isError) {
+        // @ts-ignore
+        showBoundary(error?.status);
       }
+
+      setTimeout(() => navigate(Urls.BASE.INDEX), 1000);
     };
 
-    getToken();
+    if (code) {
+      getToken();
+    }
   }, [code]);
 
-  return (
-    <section className={style.layout}>
-      <div className={style.container}>
-        <h2 className={style.title}>Welcome!</h2>
-      </div>
-    </section>
-  );
+  return (isLoading ? <Preloader /> : <WelcomeBlock />);
 }
