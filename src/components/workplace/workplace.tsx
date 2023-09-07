39e7399a007074f9dd19cd3dfe32342f8c7b3c40
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-07 08:23:56 +0300

    fix: workplace

--- src/components/workplace/workplace.tsx
+++ src/components/workplace/workplace.tsx
@@ -3,31 +3,11 @@ import React, { useEffect } from 'react';
 import { Outlet, useParams } from 'react-router';
 import { useErrorBoundary } from 'react-error-boundary';
 
-import Blocks from '../blocks';
-import WorkplaceHeader from '../workplace-header';
-import WorkplaceTools from '../workplace-tools';
-
-import { useBook, useBooks, useAppDispatch } from '../../hooks';
+import { useBooks, useAppDispatch } from '../../hooks';
 import { setCurrentBook } from '../../store/slices';
 import { useGetBlocksByIdMutation } from '../../store/api';
 
 import style from './workplace.module.css';
-
-export function Project() {
-  const data = useBook();
-
-  return (
-    <>
-      {data && (
-        <>
-          <WorkplaceHeader />
-          <Blocks />
-          <WorkplaceTools />
-        </>
-      )}
-    </>
-  );
-}
 
 export default function Workplace() {
   const dispatch = useAppDispatch();
