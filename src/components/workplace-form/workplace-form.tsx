c3c6a6cb2accfdaea911616236ab2ce19e12e396
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-07 08:24:48 +0300

    fix: workplace-form

--- src/components/workplace-form/workplace-form.tsx
+++ src/components/workplace-form/workplace-form.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import useBook from '../../hooks/use-book';
 
-import Settings from '../../layouts/settings-layout';
+import Settings from '../../layouts/settings-layout/settings-layout';
 
 // import style from './workplace-form.module.css';
 
