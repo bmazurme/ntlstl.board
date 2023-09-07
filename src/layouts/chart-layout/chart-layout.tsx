e2e87dd9c9ba993e9ef6df55e1e0de8f7ed37235
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-07 08:22:43 +0300

    fix: chart-layout

--- src/layouts/chart-layout/chart-layout.tsx
+++ src/layouts/chart-layout/chart-layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ChartPie from '../../components/chart-pie';
+import ChartPie from '../../components/chart-pie/chart-pie';
 
 export default function ChartLayout() {
   return (<ChartPie />);
