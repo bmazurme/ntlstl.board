e69bea4250fdcbe2217f30c3d9a54c298328abdd
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-07 08:10:42 +0300

    fix: logo

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { RectangleGroupIcon } from '@heroicons/react/24/outline';
 
-import IconButton from '../icon-button';
+import IconButton from '../icon-button/icon-button';
 
 import style from './logo.module.css';
 
