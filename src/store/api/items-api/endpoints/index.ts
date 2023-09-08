b6640212016c4ed2cf24e5fadf6a3f8eb95b4394
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-08 07:56:05 +0300

    fix: items-api

--- src/store/api/items-api/endpoints/index.ts
+++ src/store/api/items-api/endpoints/index.ts
@@ -1,12 +1,16 @@
 import itemsApi from '..';
 
+type TypeItemProps = { bookId: string | undefined; index: number; blockId: string; };
+type TypeItemsRemoveProps = { block: number; id: string; bookId: string | undefined; };
+type TypeItemsChangeProps = { itemId: string; itemType: string; bookId: string | undefined; };
+
 const itemsApiEndpoints = itemsApi
   .enhanceEndpoints({
     addTagTypes: ['items'],
   })
   .injectEndpoints({
     endpoints: (builder) => ({
-      removeItem: builder.mutation<TypeBlock, any>({
+      removeItem: builder.mutation<TypeBlock, TypeItemsRemoveProps>({
         query: (data) => ({
           url: '/api/items',
           method: 'DELETE',
@@ -14,7 +18,7 @@ const itemsApiEndpoints = itemsApi
         }),
         invalidatesTags: ['items'],
       }),
-      setMovedItem: builder.mutation<TypeBlock, any>({
+      setMovedItem: builder.mutation<TypeBlock, { data: TypeBlock; bookId: string; }>({
         query: (data) => ({
           url: '/api/items',
           method: 'PUT',
@@ -22,7 +26,7 @@ const itemsApiEndpoints = itemsApi
         }),
         invalidatesTags: ['items'],
       }),
-      changeItemValue: builder.mutation<TypeBlock, any>({
+      changeItemValue: builder.mutation<TypeBlock, TypeItemsChangeProps>({
         query: (data) => ({
           url: '/api/items',
           method: 'PATCH',
@@ -30,15 +34,15 @@ const itemsApiEndpoints = itemsApi
         }),
         invalidatesTags: ['items'],
       }),
-      getItemResult: builder.mutation<TypeBlock, any>({
-        query: (data) => ({
-          url: '/api/items/result',
-          method: 'PATCH',
-          body: data,
-        }),
-        invalidatesTags: ['items'],
-      }),
-      addItem: builder.mutation<TypeBlock, any>({
+      // getItemResult: builder.mutation<TypeBlock, any>({
+      //   query: (data) => ({
+      //     url: '/api/items/result',
+      //     method: 'PATCH',
+      //     body: data,
+      //   }),
+      //   invalidatesTags: ['items'],
+      // }),
+      addItem: builder.mutation<TypeBlock, TypeItemProps>({
         query: (data) => ({
           url: '/api/items',
           method: 'POST',
@@ -53,7 +57,7 @@ export const {
   useRemoveItemMutation,
   useSetMovedItemMutation,
   useChangeItemValueMutation,
-  useGetItemResultMutation,
+  // useGetItemResultMutation,
   useAddItemMutation,
 } = itemsApiEndpoints;
 export { itemsApiEndpoints };
