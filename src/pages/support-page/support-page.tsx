9705afba221b6146bd774c9e4947dcaf3be6dac2
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-08 07:52:49 +0300

    fix: support-page

--- src/pages/support-page/support-page.tsx
+++ src/pages/support-page/support-page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Support from '../../layouts/support-layout/support-layout';
+import Support from '../../layouts/support-layout';
 
 export default function SupportPage() {
   return (<Support />);
