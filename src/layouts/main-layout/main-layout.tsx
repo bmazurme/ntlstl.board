1d7a165c13e03067975645a9f764945bfede00bc
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-11 08:10:39 +0300

    fix: main-layout

--- src/layouts/main-layout/main-layout.tsx
+++ src/layouts/main-layout/main-layout.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import Container from '../../components/container';
-import Workplace from '../../components/workplace';
+import Main from '../../components/main';
 
 import style from './main-layout.module.css';
 
 export default function MainLayout() {
   return (
     <div className={style.app}>
-      <Container sidebar header children={<Workplace />} />
+      <Container header children={<Main />} />
     </div>
   );
 }
